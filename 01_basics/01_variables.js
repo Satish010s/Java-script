d5fdebe8bc235b132ce395b96cfff76651d95c9b
Satish010s <satishkumaryadav9905@gmail.com>
2025-08-19 10:14:27 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const regNo="24105128036"
+let EmailID="satishkumaryadav9905@gamilcom"
+var college ="BPMCE  Madehpura"
+ console.log([regNo,EmailID,college])
+//regNO=24105128033// not allowed to change the cont variable
+
+//we can change the value of let and var variales
+//like
+EmailID="singhaniasattishray@gmail.com"
+college="BP mandal college madhepura"
+console.log([regNo,EmailID,college])
