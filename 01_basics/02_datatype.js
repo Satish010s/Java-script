e708a97e09bf59171d62c546b6326dde402a2e36
Satish010s <satishkumaryadav9905@gmail.com>
2025-08-19 17:44:46 +0000

    datatype

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,26 @@
+"use struct"  //This means that treat js code as newer version
+//alert(3+3)  we are using node.js not browser
+// (means it will use during coding in browser)
+console.log(3+3)
+console.log("satish kumar")
+//variable are given below
+let name="satish kumar yadav"
+let age=18
+let islogedin=true
+//datatypes are:
+//number=> 2to the power 53
+//Bigint
+//string=.""
+//boolean=> true/false
+//null=>standalone value
+//undefined=.
+//symbol=>represent to unique
+//
+
+
+//object
+console.log(typeof name) //output-> string
+console.log(typeof age) //output-> number
+console.log(typeof islogedin)  //output-> boolean
+console.log(typeof null)  //output-> object
+console.log(typeof undefined)  //output-> undefined
