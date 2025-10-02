c7b460235c451330d7bec630cdb6d000755f4bb5
Satish010s <satishkumaryadav9905@gmail.com>
2025-10-02 18:54:45 +0530

    operator

---
+++ Level-2/operation.js
@@ -0,0 +1,21 @@
+let a=80
+let b=45
+console.log("addition is ",a+b)
+console.log("subtraction  is ",a-b)
+console.log("Multiplication is ",a*b)
+console.log("Division is ",a/b)
+console.log("Modulus is ",a%b)
+
+// increment and decrement
+
+console.log("Increment is ",a++)
+console.log("Increment is ",a)
+console.log("Decrement is ",b--)
+console.log("Decrement  is ",b)
+
+console.log(a+=20) //a=a+20
+console.log(b-=20) //b=b-20
+console.log(a*=2) //a=a*2
+console.log(b/=2) //b=b/2
+console.log(a**=2) //a=a**2   exponential operator
+console.log(a%=2) //a=a%2
