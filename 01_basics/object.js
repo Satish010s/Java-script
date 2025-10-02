3d314079c10f21be03ff0b560ec21a2f92c488fc
Satish010s <satishkumaryadav9905@gmail.com>
2025-10-02 17:33:45 +0530

    object in javascript

---
+++ 01_basics/object.js
@@ -0,0 +1,15 @@
+//writing objects in javascript
+//  key: value
+const item={
+     diaryMilk:'30',  //must include comma(,) after defining 
+     kitkat:"10"      //on item
+}
+console.log(item) // it acces the key and value both
+console.log(item["kitkat"]) // for accesing the value of key we write the name of key in double quotesin the class
+item["kissme"]="45" // adding new key and value to the object
+item["kitkat"]="12" //updating the value of key
+
+// you can see there i defined const to the item of object
+// so in object we chahnge the key and value 
+
+//But  if we defined the the variables wth const then we can;;t change the value 
