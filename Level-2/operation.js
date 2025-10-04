d9510073514df2943d9fa9f73d5fc4562433bbfd
Satish010s <satishkumaryadav9905@gmail.com>
2025-10-04 12:37:58 +0530

    All Operators

--- Level-2/operation.js
+++ Level-2/operation.js
@@ -7,6 +7,7 @@ console.log("Division is ",a/b)
 console.log("Modulus is ",a%b)
 
 // increment and decrement
+console.log("Increment and Decrement")
 
 console.log("Increment is ",a++)
 console.log("Increment is ",a)
@@ -19,3 +20,37 @@ console.log(a*=2) //a=a*2
 console.log(b/=2) //b=b/2
 console.log(a**=2) //a=a**2   exponential operator
 console.log(a%=2) //a=a%2
+
+// comparision operator
+console.log("Comparision operator")
+
+let x=45
+let y=65
+console.log(x==y)  //  false
+console.log(x!=y)  // True
+console.log(x===y)  //False (due to diffenerent data type)
+console.log(x!==y) //True  
+console.log(x>y) //False
+console.log(x<y) //True
+console.log(x>=y) //False
+console.log(x<=y) //True
+
+//logical operator
+console.log("Logical operator")
+
+console.log(x>y && y!=x) //False
+console.log(x<y && y!=x)  //True
+console.log(x<y || y==x) //False
+console.log(x>y || y!=x) ///True
+console.log(!(x>y)) //True
+console.log(!(x<y)) //False
+console.log(!!(x<y)) //True
+console.log(!!(x>y)) //False
+console.log(!!(x==y)) //False
+console.log(!!(x!=y)) //True
+
+//Ternary Operator
+console.log("Ternary Operator")
+let age=17
+let result=(age>=18) ? "Eligible for vote" : " Not Eligible for vote"
+console.log(result)
