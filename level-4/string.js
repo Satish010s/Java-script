12da1e82fe0e23a0656bde460cdc31000607af6c
Satish010s <satishkumaryadav9905@gmail.com>
2025-10-14 10:53:46 +0530

    string

---
+++ level-4/string.js
@@ -0,0 +1,26 @@
+// let str="satish kumar";
+// console.log(str.length) //length of string
+// console.log(str.toUpperCase())//convetinng to upper case
+// console.log(str.toLowerCase())//converting to lower case
+// console.log(str.includes("yadav")) //to check whether the string contains a particular substing or not
+// console.log(str.startsWith("sa")) // to check the contains a particular substring at  the starting or not
+// console.log(str.endsWith("ar")) // to check the contains a particular substring at  the ending or not
+// console.log(str.indexOf("kumar")) // to find the index of the substring in the string
+// console.log(str.replace("satish","rahul")) // to replace a substring with another substring
+// console.log(str.slice(0,6)) // to extract a part of the string from start index to end index-1
+// console.log(str.split(" ")) // to split the string into an array of substrings based on a specified separator   
+// console.log(str.trim()) // to remove whitespace from both ends of the string
+
+// console.log(str.charAt(4)) // to get the character at a specific index in the string
+// console.log(str.charCodeAt(4)) // to get the Unicode value of the character at a specific index in the string
+
+
+// console.log(str.concat(" yadav")) // to concatenate two or more strings together
+// console.log(str.repeat(3)) // to repeat the string a specified number of times
+
+//string template 
+let name ="satish"
+let age=24
+let city="bangalore"
+let str=`my name is ${name}.i am ${age} years old.i live in ${city}`
+console.log(str)
